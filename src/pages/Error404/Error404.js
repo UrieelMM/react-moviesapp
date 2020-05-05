345b96634c3d7c2b58b3fd6a986487321469de07
Urieel Mejia <uriel.mejia.unitec@outlook.com>
2020-05-05 16:41:26 -0500

    Commit react moviesapp

--- src/pages/Error404/Error404.js
+++ src/pages/Error404/Error404.js
@@ -9,7 +9,7 @@ const Error404 = () => {
       <h1>Error 404</h1>
       <p>Página no encontrada</p>
       <Link to="/">
-        <p>Volver al inicio</p>
+        <p>Volver a Home</p>
         <img src={error404} alt="error 404" className="error404-image" />
       </Link>
     </div>
